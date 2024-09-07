ac25db5b374d8154b280f868069e407fc2cd4005
CybermanXD <149437762+CybermanXD@users.noreply.github.com>
2024-09-08 02:49:39 +0530

    Update env.js

--- netlify/functions/env.js
+++ netlify/functions/env.js
@@ -1,8 +1,15 @@
 // netlify/functions/env.js
 exports.handler = async function(event, context) {
+    if (!process.env.GOOGLE_API_KEY) {
+        return {
+            statusCode: 500,
+            body: JSON.stringify({ error: "GOOGLE_API_KEY is not set" }),
+        };
+    }
+    
     return {
         statusCode: 200,
         body: JSON.stringify({ GOOGLE_API_KEY: process.env.GOOGLE_API_KEY }),
     };
 };
- 
+
